d0dd62fcfc39a82b8609a1b76047f9186f5bebf9
Angela Zhang <angela.zhang@dali.dartmouth.edu>
2021-12-22 13:21:48 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Angela's Personal Portfolio
+
+## Description and Credits
+This portfolio is forked from [tbakerx's react resume template](https://github.com/tbakerx/react-resume-template). Besides personalizing the content, I used [particles-bg](https://github.com/lindelof/particles-bg), a react background particle library to make the home page more dynamic.
+
+### What I Hope to Implement In the Future
+- hosting on my own domain (not just github pages)
+- a filtering system for my projects based on category
+- popup / expanding effect when hovering over my projects
+
+## Installing and Running the Project
+The live link to the website can be accessed [here](https://azhang4216.github.io/portfolio/).
+
+### 1. Clone the project
+```shell
+git clone https://github.com/azhang4216/portfolio.git
+```
+
+### 2. Run the project
+```shell
+npm i
+npm start
+```
+
+### 3. Build
+```shell
+npm run build
+```
---
+++ src/Components/Portfolio.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import Zmage from "react-zmage";
+import Fade from "react-reveal";
+
+let id = 0;
+class Portfolio extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const projects = this.props.data.projects.map(function (projects) {
+      let projectImage = "images/portfolio/" + projects.image;
+      let letters = "0123456789ABCDEF";
+      let backgroundColor = "#";
+      for (let i = 0; i < 6; i++) {
+        backgroundColor += letters[Math.floor(Math.random() * 16)];
+      }
+
+      return (
+        <div key={id++} className="columns portfolio-item">
+          <div className="item-wrap" onClick={()=> window.open(projects.url, "_blank")}>
+            <Zmage alt={projects.title} src={projectImage} style={{"object-fit": "cover"}}/>
+            <div class="image__overlay image__overlay--primary" style={{backgroundColor}}>
+              <div class="image__title">{projects.categories}</div>
+              <p class="image__description">
+                {projects.description}
+              </p>
+            </div>
+            <div style={{ textAlign: "center" }}>{projects.title}</div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <section id="portfolio">
+        <Fade left duration={1000} distance="40px">
+          <div className="row">
+            <div className="twelve columns collapsed">
+              <h1>Check Out Some of My Works.</h1>
+
+              <div
+                id="portfolio-wrapper"
+                className="bgrid-thirds s-bgrid-thirds cf"
+              >
+                {projects}
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
